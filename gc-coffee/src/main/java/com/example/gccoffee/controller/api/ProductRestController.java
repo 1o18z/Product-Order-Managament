b61fc7ef7e85fd172198fc99e4bc561f25dccd53
박은지 <qkrdmswl1018@naver.com>
2023-07-18 23:57:31 +0900

    refactor: ProductRestController 구현

--- gc-coffee/src/main/java/com/example/gccoffee/controller/api/ProductRestController.java
+++ gc-coffee/src/main/java/com/example/gccoffee/controller/api/ProductRestController.java
@@ -1,16 +1,19 @@
 package com.example.gccoffee.controller.api;
 
+import com.example.gccoffee.dto.product.ProductCreateDto;
+import com.example.gccoffee.dto.product.ProductResponseDto;
+import com.example.gccoffee.dto.product.ProductUpdateDto;
 import com.example.gccoffee.model.Category;
-import com.example.gccoffee.model.Product;
 import com.example.gccoffee.service.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/product")
 public class ProductRestController {
 
   private final ProductService productService;
@@ -19,11 +22,54 @@ public class ProductRestController {
     this.productService = productService;
   }
 
-  @GetMapping("/api/v1/products")
-  public List<Product> productList(@RequestParam Optional<Category> category) { // RequestParam 이용해서 카테고리 별로 조회하게 할 수도
-    return category
-            .map(productService::getProductCategory)
-            .orElse(productService.getAllProducts());
+  @PostMapping("/create")
+  public ResponseEntity<ProductResponseDto> create(@RequestBody ProductCreateDto createProductRequest) {
+    ProductResponseDto responseDto = productService.create(createProductRequest);
+    return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+  }
+
+  @GetMapping("/{productId}/update")
+  public String updateForm(@PathVariable UUID productId) {
+    return "products/update-product";
+  }
+
+  @PutMapping("/{productId}/update")
+  public ResponseEntity<ProductResponseDto> update(@PathVariable UUID productId, ProductUpdateDto productUpdateDto) {
+    ProductResponseDto responseDto = productService.update(productUpdateDto);
+    return new ResponseEntity<>(responseDto, HttpStatus.OK);
+  }
+
+  @GetMapping("/list")
+  public ResponseEntity<List<ProductResponseDto>> findAll() {
+    List<ProductResponseDto> responseDtoList = productService.findAll();
+    return new ResponseEntity<>(responseDtoList, HttpStatus.OK);
+  }
+
+  @GetMapping("/{productId}")
+  public ResponseEntity<ProductResponseDto> findById(@PathVariable UUID productId) {
+    ProductResponseDto responseDto = productService.findById(productId);
+    return new ResponseEntity<>(responseDto, HttpStatus.OK);
+  }
+
+  @GetMapping("/{productId}")
+  public ResponseEntity<ProductResponseDto> findByName(@RequestParam(value = "productId") String name) {
+    ProductResponseDto responseDto = productService.findByName(name);
+    return new ResponseEntity<>(responseDto, HttpStatus.OK);
+  }
+
+  @GetMapping("/list")
+  public List<ProductResponseDto> findByCategory(Category category) {
+    return productService.findByCategory(category);
+  }
+
+  @DeleteMapping("/list")
+  public void deleteAll() {
+    productService.deleteAll();
+  }
+
+  @DeleteMapping("/{productId}")
+  public void deleteById(UUID productId) {
+    productService.deleteById(productId);
   }
 
 }
