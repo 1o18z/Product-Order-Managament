e269f614ffc3bef2d2bfc28e20e5c94ceeac2173
박은지 <qkrdmswl1018@naver.com>
2023-07-19 00:04:32 +0900

    refactor: OrderRestController 구현

--- gc-coffee/src/main/java/com/example/gccoffee/controller/api/OrderRestController.java
+++ gc-coffee/src/main/java/com/example/gccoffee/controller/api/OrderRestController.java
@@ -1,13 +1,17 @@
 package com.example.gccoffee.controller.api;
 
-import com.example.gccoffee.controller.CreateOrderRequest;
-import com.example.gccoffee.model.Email;
-import com.example.gccoffee.model.Order;
+import com.example.gccoffee.dto.order.OrderCreateDto;
+import com.example.gccoffee.dto.order.OrderResponseDto;
 import com.example.gccoffee.service.OrderService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/order")
 public class OrderRestController {
 
   private final OrderService orderService;
@@ -16,14 +20,27 @@ public class OrderRestController {
     this.orderService = orderService;
   }
 
-  @GetMapping("api/v1/orders")
-  public Order createOrder(CreateOrderRequest orderRequest) {
-    return orderService.createOrder(
-            new Email(orderRequest.email()),
-            orderRequest.address(),
-            orderRequest.postcode(),
-            orderRequest.orderItems()
-    );
+  @PostMapping("/create")
+  public ResponseEntity<OrderResponseDto> createOrder(@RequestBody OrderCreateDto orderCreateDto) {
+    OrderResponseDto responseDto = orderService.create(orderCreateDto);
+    return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+  }
+
+  @GetMapping("/list")
+  public ResponseEntity<List<OrderResponseDto>> findAll() {
+    List<OrderResponseDto> responseDtoList = orderService.findAll();
+    return new ResponseEntity<>(responseDtoList, HttpStatus.OK);
+  }
+
+  @GetMapping("/{orderId}")
+  public ResponseEntity<OrderResponseDto> findById(@PathVariable UUID orderId) {
+    OrderResponseDto responseDto = orderService.findById(orderId);
+    return new ResponseEntity<>(responseDto, HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{id}")
+  public void cancel(UUID orderId) {
+    orderService.cancel(orderId);
   }
 
 }
