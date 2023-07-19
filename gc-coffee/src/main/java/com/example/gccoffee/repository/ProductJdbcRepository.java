03ed4b811d2520d20342b3cb861851cc1d8c7667
박은지 <qkrdmswl1018@naver.com>
2023-07-19 17:35:33 +0900

    refactor: PrductJdbcRepository 수정 및 테스트

--- gc-coffee/src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
+++ gc-coffee/src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
@@ -3,12 +3,11 @@ package com.example.gccoffee.repository;
 import com.example.gccoffee.dto.product.ProductUpdateDto;
 import com.example.gccoffee.model.Category;
 import com.example.gccoffee.model.Product;
-import com.example.gccoffee.query.SelectQuery;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
@@ -18,11 +17,9 @@ import java.util.List;
 public class ProductJdbcRepository implements ProductRepository {
 
   private final NamedParameterJdbcTemplate jdbcTemplate;
-  private final SelectQuery selectQuery;
 
-  public ProductJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate, SelectQuery selectQuery) {
+  public ProductJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
     this.jdbcTemplate = jdbcTemplate;
-    this.selectQuery = selectQuery;
   }
 
   @Override
@@ -32,7 +29,7 @@ public class ProductJdbcRepository implements ProductRepository {
 
   @Override
   public Product insert(Product product) {
-    MapSqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+    SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
             .addValue("productId", product.getProductId())
             .addValue("productName", product.getName())
             .addValue("category", product.getCategory().toString())
@@ -50,9 +47,14 @@ public class ProductJdbcRepository implements ProductRepository {
 
   @Override
   public Product update(ProductUpdateDto productUpdateDto) {
-    BeanPropertySqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(productUpdateDto);
-    int updatedProduct = jdbcTemplate.update("UPDATE products SET product_name, category, price, description" +
-            "WHERE product_id = :productId", sqlParameterSource
+    SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+            .addValue("productId", productUpdateDto.productId())
+            .addValue("productName", productUpdateDto.productName())
+            .addValue("category", productUpdateDto.category().toString())
+            .addValue("price", productUpdateDto.price())
+            .addValue("description", productUpdateDto.description()
+            );
+    int updatedProduct = jdbcTemplate.update("UPDATE products SET product_name = :productName, category = :category, price = :price, description = :description WHERE product_id = :productId", sqlParameterSource
     );
     if (updatedProduct != 1) {
       throw new RuntimeException("Nothing was updated");
@@ -65,7 +67,7 @@ public class ProductJdbcRepository implements ProductRepository {
     try {
       return Optional.ofNullable(
               jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_id = :productId",
-                      Collections.singletonMap("productId", productId.toString().getBytes()), productRowMapper)
+                      Collections.singletonMap("productId", productId.toString()), productRowMapper)
       );
     } catch (EmptyResultDataAccessException e) {
       return Optional.empty();
@@ -76,7 +78,7 @@ public class ProductJdbcRepository implements ProductRepository {
   public Optional<Product> findByName(String productName) {
     try {
       return Optional.ofNullable(
-              jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_id = :productId",
+              jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_name = :productName",
                       Collections.singletonMap("productName", productName), productRowMapper)
       );
     } catch (EmptyResultDataAccessException e) {
@@ -87,25 +89,25 @@ public class ProductJdbcRepository implements ProductRepository {
   @Override
   public List<Product> findByCategory(Category category) {
     return jdbcTemplate.query("SELECT * FROM products WHERE category = :category",
-            Collections.singletonMap("category", category),
+            Collections.singletonMap("category", category.toString()),
             productRowMapper
     );
   }
 
   @Override
   public void deleteAll() {
-    jdbcTemplate.update("DELETE * from products", Collections.emptyMap());
+    jdbcTemplate.update("DELETE from products", Collections.emptyMap());
   }
 
   @Override
   public void deleteById(UUID productId) {
-    jdbcTemplate.update("DELETE * from products WHERE product_id = :productId", Collections.emptyMap());
+    jdbcTemplate.update("DELETE from products WHERE product_id = :productId", Collections.emptyMap());
   }
 
-  private static final RowMapper<Product> productRowMapper = (resultSet, rowNum) -> {
+  private final RowMapper<Product> productRowMapper = (resultSet, rowNum) -> {
     Product product = new Product(
-            UUID.fromString(resultSet.getString("productId")),
-            resultSet.getString("productName"),
+            UUID.fromString(resultSet.getString("product_id")),
+            resultSet.getString("product_name"),
             Category.valueOf(resultSet.getString("category")),
             resultSet.getInt("price"),
             resultSet.getString("description")
--- gc-coffee/src/test/java/com/example/gccoffee/model/ProductJdbcRepositoryTest.java
+++ gc-coffee/src/test/java/com/example/gccoffee/model/ProductJdbcRepositoryTest.java
@@ -1,76 +1,113 @@
 package com.example.gccoffee.model;
 
-import com.example.gccoffee.dto.product.ProductMapper;
-import com.example.gccoffee.dto.product.ProductResponseDto;
 import com.example.gccoffee.dto.product.ProductUpdateDto;
 import com.example.gccoffee.repository.ProductJdbcRepository;
-import com.example.gccoffee.service.ProductService;
+import com.example.gccoffee.validator.ProductValidator;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootTest
 class ProductJdbcRepositoryTest {
 
-  private final ProductJdbcRepository productJdbcRepository;
-  private final ProductMapper productMapper;
-  private final ProductService productService;
+  @Autowired
+  ProductJdbcRepository productJdbcRepository;
 
-  public ProductJdbcRepositoryTest(ProductJdbcRepository productJdbcRepository, ProductMapper productMapper, ProductService productService) {
-    this.productJdbcRepository = productJdbcRepository;
-    this.productMapper = productMapper;
-    this.productService = productService;
+  private final Product firstProduct = new Product(UUID.randomUUID(), "아메리카노", Category.COFFEE, 5000, "아메리카농농농");
+  private final Product secondProduct = new Product(UUID.randomUUID(), "티라미수", Category.COFFEE_DESSERT, 7000, "티라미숭숭숭");
+  private final Product thridProduct = new Product(UUID.randomUUID(), "라떼", Category.COFFEE, 5500, "라뗑뗑뗑");
+  private static final int TEST_RESULT_SIZE = 2;
+  private static final int DELETE_TEST_RESULT_SIZE = 0;
+
+
+  @BeforeEach
+  void delete() {
+    productJdbcRepository.deleteAll();
   }
-
-  private static final Product newProduct = new Product(UUID.randomUUID(), "아메리카노", Category.COFFEE, 5000, "아메리카농농농");
-  private static final int INSERT_TEST_RESULT_SIZE = 1;
 
   @Test
   @DisplayName("상품을 추가할 수 있다.")
   void insertProduct_Success() {
-    productJdbcRepository.insert(newProduct);
-    var all = productJdbcRepository.findAll();
-    Assertions.assertEquals(all.size(), INSERT_TEST_RESULT_SIZE);
+    productJdbcRepository.insert(firstProduct);
+    productJdbcRepository.insert(secondProduct);
+
+    List<Product> products = productJdbcRepository.findAll();
+
+    Assertions.assertEquals(products.size(), TEST_RESULT_SIZE);
   }
 
   @Test
   @DisplayName("상품을 이름으로 조회할 수 있다.")
   void findByName_Success() {
-    var product = productJdbcRepository.findByName(newProduct.getName());
-    Assertions.assertFalse(product.isEmpty());
+    productJdbcRepository.insert(secondProduct);
+
+    Optional<Product> findProduct = productJdbcRepository.findByName(secondProduct.getName());
+    ProductValidator.checkExist(findProduct);
+    Product product = findProduct.get();
+
+    Assertions.assertEquals(product.getProductId(), secondProduct.getProductId());
   }
 
   @Test
   @DisplayName("상품을 아이디로 조회할 수 있다.")
   void findById_Success() {
-    var product = productJdbcRepository.findById(newProduct.getProductId());
-    Assertions.assertFalse(product.isEmpty());
+    productJdbcRepository.insert(firstProduct);
+    productJdbcRepository.insert(secondProduct);
+
+    Optional<Product> findProduct = productJdbcRepository.findById(firstProduct.getProductId());
+    ProductValidator.checkExist(findProduct);
+    Product product = findProduct.get();
+
+    Assertions.assertEquals(product.getName(), firstProduct.getName());
   }
 
   @Test
   @DisplayName("상품을 카테고리로 조회할 수 있다.")
   void findByCategory_Success() {
-    var product = productJdbcRepository.findByCategory(Category.COFFEE_BEAN);
-    Assertions.assertFalse(product.isEmpty());
+    Category category = Category.COFFEE;
+    productJdbcRepository.insert(firstProduct);
+    productJdbcRepository.insert(secondProduct);
+    productJdbcRepository.insert(thridProduct);
+
+    List<Product> findCategory = productJdbcRepository.findByCategory(category);
+
+    Assertions.assertEquals(findCategory.size(), TEST_RESULT_SIZE);
+    Assertions.assertEquals(category, findCategory.get(0).getCategory());
+    Assertions.assertEquals(category, findCategory.get(1).getCategory());
   }
 
   @Test
   @DisplayName("상품을 수정할 수 있다.")
   void updateProduct_Success() {
-    ProductUpdateDto updateDto = new ProductUpdateDto(newProduct.getProductId(), "커피콩빵", Category.COFFEE_DESSERT, 3500, "빵빵빵");
-    ProductResponseDto responseDto = productService.update(updateDto);
-    Assertions.assertEquals(updateDto, responseDto);
+    productJdbcRepository.insert(firstProduct);
+
+    ProductUpdateDto updateDto = new ProductUpdateDto(firstProduct.getProductId(), "커피콩빵", Category.COFFEE_DESSERT, 3500, "빵빵빵");
+    Product product = productJdbcRepository.update(updateDto);
+
+    Assertions.assertNotEquals(firstProduct.getName(), product.getName());
+    Assertions.assertNotEquals(firstProduct.getCategory(), product.getCategory());
   }
 
   @Test
   @DisplayName("상품을 전체 삭제한다.")
   void testDeleteAll() {
+    productJdbcRepository.insert(firstProduct);
+    productJdbcRepository.insert(secondProduct);
+    int beforeSize = productJdbcRepository.findAll().size();
+    Assertions.assertEquals(beforeSize, TEST_RESULT_SIZE);
+
     productJdbcRepository.deleteAll();
-    var all = productJdbcRepository.findAll();
-    Assertions.assertFalse(all.isEmpty());
+    int afterSize = productJdbcRepository.findAll().size();
+    Assertions.assertEquals(afterSize, DELETE_TEST_RESULT_SIZE);
+
+    Assertions.assertNotEquals(beforeSize, afterSize);
   }
 
 }
