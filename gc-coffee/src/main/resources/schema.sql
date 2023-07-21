308a3163faec78d03584ab7f846f9243cefde113
박은지 <qkrdmswl1018@naver.com>
2023-07-21 17:50:13 +0900

    refactor: 스키마 수정

--- gc-coffee/src/main/resources/schema.sql
+++ gc-coffee/src/main/resources/schema.sql
@@ -1,14 +1,23 @@
 DROP TABLE IF EXISTS products CASCADE;
 DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS order_items CASCADE;
 
 CREATE TABLE products
 (
     product_id   VARCHAR(50) PRIMARY KEY,
     product_name VARCHAR(20) NOT NULL,
     category     VARCHAR(50) NOT NULL,
-    price        INT         NOT NULL,
-    description  VARCHAR(500) DEFAULT NULL,
-    created_at   TIMESTAMP   NOT NULL
+    price        INT         NOT NULL
+);
+
+CREATE TABLE order_items
+(
+    order_item_id VARCHAR(50) PRIMARY KEY,
+    order_id      VARCHAR(50) NOT NULL,
+    product_id    VARCHAR(50) NOT NULL,
+    quantity      INT         NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products (product_id),
+    FOREIGN KEY (order_id) REFERENCES orders (order_id)
 );
 
 CREATE TABLE orders
@@ -16,9 +25,5 @@ CREATE TABLE orders
     order_id     VARCHAR(50) PRIMARY KEY,
     email        VARCHAR(50)  NOT NULL,
     address      VARCHAR(200) NOT NULL,
-    product_id   VARCHAR(50)  NOT NULL,
-    quantity     INT          NOT NULL,
-    order_status VARCHAR(50)  NOT NULL,
-    created_at   TIMESTAMP    NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products (product_id)
+    order_status VARCHAR(50)  NOT NULL
 );
