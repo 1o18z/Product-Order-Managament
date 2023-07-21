a5e7588e7125adab3cc9b09b203aa6abf458c614
박은지 <qkrdmswl1018@naver.com>
2023-07-21 21:09:07 +0900

    refactor: OrderService에 DTO를 OrderItem로 변환하는 로직 추가

--- gc-coffee/src/main/java/com/example/gccoffee/service/OrderService.java
+++ gc-coffee/src/main/java/com/example/gccoffee/service/OrderService.java
@@ -3,12 +3,17 @@ package com.example.gccoffee.service;
 import com.example.gccoffee.dto.order.OrderCreateDto;
 import com.example.gccoffee.dto.order.OrderMapper;
 import com.example.gccoffee.dto.order.OrderResponseDto;
+import com.example.gccoffee.dto.orderItem.OrderItemCreateDto;
+import com.example.gccoffee.dto.orderItem.OrderItemMapper;
 import com.example.gccoffee.model.Order;
+import com.example.gccoffee.model.OrderItem;
+import com.example.gccoffee.repository.OrderItemRepository;
 import com.example.gccoffee.repository.OrderRepository;
 import com.example.gccoffee.validator.OrderValidator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -20,20 +25,33 @@ public class OrderService {
   private final OrderMapper orderMapper;
   private final OrderRepository orderRepository;
   private final OrderValidator orderValidator;
+  private final OrderItemMapper orderItemMapper;
+  private final OrderItemRepository orderItemRepository;
 
-  public OrderService(OrderMapper orderMapper, OrderRepository orderRepository, OrderValidator orderValidator) {
+  public OrderService(OrderMapper orderMapper, OrderRepository orderRepository, OrderValidator orderValidator, OrderItemMapper orderItemMapper, OrderItemRepository orderItemRepository) {
     this.orderMapper = orderMapper;
     this.orderRepository = orderRepository;
     this.orderValidator = orderValidator;
+    this.orderItemMapper = orderItemMapper;
+    this.orderItemRepository = orderItemRepository;
   }
 
   @Transactional
   public OrderResponseDto create(OrderCreateDto orderCreateDto) {
     orderValidator.validEmail(orderCreateDto.email());
-    orderValidator.validQuantity(orderCreateDto.quantity());
 
     Order createdOrder = orderMapper.toOrder(orderCreateDto);
     Order savedOrder = orderRepository.insert(createdOrder);
+
+    List<OrderItemCreateDto> orderItems = orderCreateDto.orderItems();
+    List<OrderItem> orderItemList = new ArrayList<>();
+
+    for (OrderItemCreateDto item : orderItems) {
+      OrderItem orderItem = orderItemMapper.toOrderItem(item, savedOrder.getOrderId());
+      orderItemList.add(orderItem);
+    }
+
+    orderItemRepository.add(orderItemList);
     return orderMapper.toResponse(savedOrder);
   }
 
