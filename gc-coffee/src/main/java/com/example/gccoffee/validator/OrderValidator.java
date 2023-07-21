0dffddbe206031abc4de6c6a1a3bc950a48433ea
박은지 <qkrdmswl1018@naver.com>
2023-07-21 10:48:45 +0900

    refactor: Validator 작성

--- gc-coffee/src/main/java/com/example/gccoffee/validator/OrderValidator.java
+++ gc-coffee/src/main/java/com/example/gccoffee/validator/OrderValidator.java
@@ -1,15 +1,61 @@
 package com.example.gccoffee.validator;
 
 import com.example.gccoffee.model.Order;
+import com.example.gccoffee.model.Product;
+import org.springframework.stereotype.Component;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.regex.Pattern;
 
+@Component
 public class OrderValidator {
 
-  public static void checkExist(Optional<Order> order){
-    if(order.isEmpty()){
+  private static final String REG_EXP_EMAIL = "\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b";
+  private static final Pattern EMAIL_PATTERN = Pattern.compile(REG_EXP_EMAIL);
+
+  private static final int MINUMUM_QUANTITY = 1;
+  private static final int MAXIMUM_QUANTITY = 50;
+
+  private static final String INVALID_ID = "유효하지 않은 ID - ID가 비어있습니다.";
+  private static final String INVALID_EMAIL = "이메일 형식이 맞지 않습니다.";
+  private static final String INVALID_QUANTITY = "1 ~ 50 범위 내의 값을 입력하세요.";
+  private static final String INVALID_ORDER = "존재하지 않는 주문입니다.";
+
+  public static void checkExist(Optional<Order> order) {
+    if (order.isEmpty()) {
       throw new NoSuchElementException("No Exist");
+    }
+  }
+
+  public static void validId(UUID productId) {
+    if (productId == null) {
+      throw new IllegalArgumentException(INVALID_ID);
+    }
+  }
+
+  public void validProduct(Optional<Product> product) {
+    if (product.isEmpty()) {
+      throw new NoSuchElementException("No Exist");
+    }
+  }
+
+  public void validEmail(String email) {
+    if (!EMAIL_PATTERN.matcher(email).matches() || email.isEmpty() || email.isBlank()) {
+      throw new IllegalArgumentException(INVALID_EMAIL);
+    }
+  }
+
+  public void validQuantity(int quantity) {
+    if (quantity < MINUMUM_QUANTITY && quantity > MAXIMUM_QUANTITY) {
+      throw new RuntimeException(INVALID_QUANTITY);
+    }
+  }
+
+  public void validOrder(Optional<Order> order) {
+    if (order.isEmpty()) {
+      throw new NoSuchElementException(INVALID_ORDER);
     }
   }
 
--- gc-coffee/src/main/java/com/example/gccoffee/validator/ProductValidator.java
+++ gc-coffee/src/main/java/com/example/gccoffee/validator/ProductValidator.java
@@ -1,15 +1,60 @@
 package com.example.gccoffee.validator;
 
 import com.example.gccoffee.model.Product;
+import com.example.gccoffee.repository.ProductRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.UUID;
 
+@Component
 public class ProductValidator {
 
-  public static void checkExist(Optional<Product> product){
-    if(product.isEmpty()){
-      throw new NoSuchElementException("No Exist");
+  private static final int MINUMUM_PRICE = 2000;
+  private static final int MAXIMUM_PRICE = 200000;
+  private static final int MINIMUM_LENGTH = 5;
+  private static final int MAXIMUM_LENGTH = 50;
+
+  private static final String INVALID_PRICE = "2000 ~ 200000 범위 내의 값을 입력하세요.";
+  private static final String INVALID_DESCRIPTION = "5 ~ 50 범위 내의 글자를 입력하세요.";
+  private static final String INVALID_ID = "유효하지 않은 ID - ID가 비어있습니다.";
+  private static final String INVALID_NAME = "이미 존재하는 이름입니다.";
+  private static final String INVALID_PRODUCT = "존재하지 않는 상품입니다.";
+
+  private final ProductRepository productRepository;
+
+  public ProductValidator(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
+  public void validName(String productName) {
+    if (productName == null || productRepository.findByName(productName).isPresent()) {
+      throw new IllegalArgumentException(INVALID_NAME);
+    }
+  }
+
+  public void validDescription(String description) {
+    if (description.length() < MINIMUM_LENGTH && description.length() > MAXIMUM_LENGTH) {
+      throw new RuntimeException(INVALID_DESCRIPTION);
+    }
+  }
+
+  public void validPrice(int price) {
+    if (price < MINUMUM_PRICE && price > MAXIMUM_PRICE) {
+      throw new RuntimeException(INVALID_PRICE);
+    }
+  }
+
+  public void validProduct(Optional<Product> product) {
+    if (product.isEmpty()) {
+      throw new NoSuchElementException(INVALID_PRODUCT);
+    }
+  }
+
+  public static void validId(UUID productId) {
+    if (productId == null) {
+      throw new IllegalArgumentException(INVALID_ID);
     }
   }
 
