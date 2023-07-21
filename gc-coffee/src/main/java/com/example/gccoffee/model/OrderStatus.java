ee68248ab904f93684cb1086ccb6d7a2e0e43577
박은지 <qkrdmswl1018@naver.com>
2023-07-21 10:52:30 +0900

    refactor: OrderStatus 수정

--- gc-coffee/src/main/java/com/example/gccoffee/model/OrderStatus.java
+++ gc-coffee/src/main/java/com/example/gccoffee/model/OrderStatus.java
@@ -1,12 +1,12 @@
 package com.example.gccoffee.model;
 
 public enum OrderStatus {
-  ORDER_SUCCESS,
-  ACCEPTED,
-  PAYMENT_CONFIRMED,
-  READY_FOR_DELIVERY,
-  SHIPPED,
-  SETTLED,
+  BASKET, // 장바구니
+  ORDER_SUCCESS, // 주문 성공
+  PAYMENT_CONFIRMED, // 결제 완료
+  READY_FOR_DELIVERY, // 상품 준비 중
+  SHIPPED, // 배송 중
+  DELIVERY_SUCCESS, // 배송 완료
   CANCELED,
   ;
 }
