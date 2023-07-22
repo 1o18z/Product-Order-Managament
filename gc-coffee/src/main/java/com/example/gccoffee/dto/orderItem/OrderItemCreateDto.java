eaac4c57ddac9e4db6dea8067036cee22c0d1bce
박은지 <qkrdmswl1018@naver.com>
2023-07-22 14:33:23 +0900

    refactor: class를 record로 변경

--- gc-coffee/src/main/java/com/example/gccoffee/dto/orderItem/OrderItemCreateDto.java
+++ gc-coffee/src/main/java/com/example/gccoffee/dto/orderItem/OrderItemCreateDto.java
@@ -2,21 +2,8 @@ package com.example.gccoffee.dto.orderItem;
 
 import java.util.UUID;
 
-public class OrderItemCreateDto {
-  private final UUID productId;
-  private final int quantity;
-
-  public OrderItemCreateDto(UUID productId, int quantity) {
-    this.productId = productId;
-    this.quantity = quantity;
-  }
-
-  public UUID getProductId() {
-    return productId;
-  }
-
-  public int getQuantity() {
-    return quantity;
-  }
-
+public record OrderItemCreateRequest(
+        UUID productId,
+        int quantity
+) {
 }
